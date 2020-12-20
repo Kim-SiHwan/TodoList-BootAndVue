111e1a4fccc9f1e4ed83510769e74a6a320c0bb6
Kim-SiHwan <tjdans4166@naver.com>
2020-12-21 01:54:08 +0900

    TodoVueComponent

---
+++ vue-frontend/src/components/TodoInput.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <input type="text" v-model="item" v-on:keyup.enter="addItem"
+    placeholder="할 일을 입력하세요!">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "TodoInput",
+  data(){
+    return{
+      item :'',
+      form :''
+    }
+  },
+  methods :{
+    addItem(){
+      this.form = {
+        item : this.item
+      };
+      if(this.item.length!=0){
+        console.log(this.form);
+        this.$emit('addItem',this.form);
+        this.clearItem();
+      }
+    },
+    clearItem(){
+      this.item='';
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-frontend/src/components/TodoList.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li v-for="(list,index) in probsdata" :key="index">
+      {{list.item}}, {{index}}
+    </li>
+  </ul>
+
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  props: ['probsdata'],
+  data(){
+    return{
+      list:''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
